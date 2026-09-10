c737a5bef5c9a428d7cd8c9e55d28e8f4d3b8335
MERYEM FARES <ashrshahom5@gmail.com>
2026-09-10 02:53:32 +0100

    Load robust mobile navigation styles

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,7 @@ $currentPage = $currentPage ?? '';
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/mobile-menu.css">
 </head>
 <body>
 <a class="skip-link" href="#main-content">Skip to content</a>
